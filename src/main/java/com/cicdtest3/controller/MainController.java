ea7790d3027e1d7592f4705c631da5b69ae41e1f
kostackdgk <kostackdgkim@naver.com>
2024-11-27 11:49:49 +0900

    first commit

---
+++ src/main/java/com/cicdtest3/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.cicdtest3.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/")
+    public String main() {
+        return "this is container 기반 CICD";
+    }
+}
